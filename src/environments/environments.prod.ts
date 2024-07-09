9ea78d3e8806038d90eccde22e55510110cd4304
Marta Ocana <martacyta2021@gmail.com>
2024-07-09 17:11:19 +0200

    create environments

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,3 @@
+export const environments = {
+  baseUrl: 'http://marta-desinforma.com/api'
+  }
---
+++ src/environments/environments.ts
@@ -0,0 +1,3 @@
+export const environments = { //aqui definimos todas las variables de entorno que necesitamos
+  baseUrl: 'http://localhost:3000'//en este caso en particular, esta baseUrl hace referencia a la base de estas url http://localhost:3000/users y http://localhost:3000/heroes que son las url que nos proporciona el servidor json-server y que contienen la informacion de nuestro backend que nos hemos creado dentro de la carpeta data en el archivo db.json
+  }
