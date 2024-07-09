3c3ce732ac086694db26704ada7a3d22d6de6f8d
Marta Ocana <martacyta2021@gmail.com>
2024-07-09 12:55:33 +0200

    upload layout-page of heroes component

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class LayoutPageComponent {
+  //opciones del menú para poder navegar entre diferentes pantallas
+  public sidebarItems = [
+    {label: 'Listado', icon: 'label', url: './list'}, //path relativo a donde me encuentro que muestra la url http://localhost:4200/heroes/list
+    {label: 'Añadir', icon: 'add', url: './new-hero'}, //path relativo a donde me encuentro que muestra la url http://localhost:4200/heroes/new-hero
+    {label: 'Buscar', icon: 'search', url: './search'}, //path relativo a donde me encuentro que muestra la url http://localhost:4200/heroes/search
+  ]
 
 }
